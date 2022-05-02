701c876fa0107156f67cf4b1e02080e8c77d8cde
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-05-02 17:56:30 +0300

    add 002 task

---
+++ 002task/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. */
+
+void Sum(int N)
+{
+    int S = 0;
+    while (N > 0 || N < 0)
+    {
+        int R = N % 10;
+        S = S + R;
+        N = N / 10;
+    }
+    Console.WriteLine(S);
+}
+
+Sum(-86359);
+Console.WriteLine();
