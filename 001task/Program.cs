adaa45ee608318b99fa30fe4137f2cc583ff895d
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-05-02 17:51:07 +0300

    add 001 task

---
+++ 001task/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. */
+
+void IfNegative(double A, double B)
+{
+    double res = 0;
+
+    if (A > 0 && B > 0)
+    {
+        res = Math.Pow(A, B);
+    }
+    if (A < 0 && B < 0)
+    {
+        res = Math.Pow(A, (B * -1));
+    }
+    if (A > 0 && B < 0)
+    {
+        res = Math.Pow(A, (B * -1));
+    }
+    if (A < 0 && B > 0)
+    {
+        res = Math.Pow(A, B);
+    }
+
+    Console.WriteLine(res);
+}
+
+IfNegative(-2, -5);
+Console.WriteLine();
