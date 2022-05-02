a68b27c4a269342e245a40fe7dd9d2edd6e67786
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-05-02 18:20:18 +0300

    add 004 task

---
+++ 004task/Program.cs
@@ -0,0 +1,14 @@
+﻿/* ОБЯЗАТЕЛЬНАЯ ЗАДАЧА: Найти сумму чисел от 1 до А. */
+
+void Sum(int A)
+{
+    int S = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        S = S + i;
+    }
+    Console.WriteLine(S);
+}
+
+Sum(16);
+Console.WriteLine();
