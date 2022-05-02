3c2edcf0057eae139bada15e1554c5cd9d5e43cb
Alexandra Gorlova <gorlova.alexandra2014@yandex.ru>
2022-05-02 17:59:07 +0300

    add 003 task

---
+++ 003task/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+
+void FillArray(int[] array)
+{
+    int lenght = array.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        array[index] = new Random().Next(-99, 100);
+        Console.Write(array[index] + " ");
+        index++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+Console.WriteLine();
